fca7b899cfd3f851464c8ec4f8ef62db62907271
Alfie Thomasson <alfiethomasson0@gmail.com>
2020-10-18 22:28:43 +0100

    Updating CMAKE settings, and adding empty files for grid class

---
+++ App/Main.cpp
@@ -0,0 +1,19 @@
+#include <thread>
+#include <iostream>
+
+using namespace std;
+
+void hello_world()
+{
+	cout << "Hello from thread " << this_thread::get_id() << endl;
+}
+
+int main(int argc, char** argv)
+{
+	// Create a new thread
+	thread t(hello_world);
+	// Wait for thread to finish (join it)
+	t.join();
+	// Return 0 (OK)
+	return 0;
+}
